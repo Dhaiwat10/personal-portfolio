0b72630994cdc1080f799465dc5a1b9d9885126b
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2021-04-07 16:27:18 +0530

    Add links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 const INPUT =
-  "> Hi!👋 I'm Dhaiwat. I am a 20-year-old freelance software engineer.\n\n> ⚡️ I am highly proficient with:\n- React\n- TypeScript\n- AWS\n- Solidity\n\n> 🔨 What I can build for you:\n- A full-stack web app\n- An ethereum smart contract\n- A Chrome extension\n- A Discord bot\n\n> Impressed? Let's talk.";
+  "> Hi!👋 I'm Dhaiwat. I am a 20-year-old freelance software engineer.\n\n> ⚡️ I am highly proficient with:\n- React\n- TypeScript\n- AWS\n- Solidity\n\n> ❤️ Live projects that I have worked on:\n% \n\n> 🔨 What I can build for you:\n- A full-stack web app\n- An ethereum smart contract\n- A Chrome extension\n- A Discord bot\n\n> Impressed? Let's talk! 👇🏼";
 
 // markup
 const IndexPage = () => {
@@ -36,10 +36,68 @@ const IndexPage = () => {
                   <br />
                 </>
               );
+            } else if (item === '%') {
+              return (
+                <div style={{ marginBottom: '-18px' }} className="fadeIn">
+                  <span>- </span>
+                  <a
+                    className="underline"
+                    href="https://projectlockdown.world/"
+                  >
+                    Project Lockdown
+                  </a>
+                  <br />
+
+                  <span>- </span>
+                  <a className="underline" href="https://barcauniversal.com">
+                    Barça Universal
+                  </a>
+                  <br />
+
+                  <span>- </span>
+                  <a className="underline" href="https://vofis.vercel.app">
+                    Vof.Is
+                  </a>
+                  <br />
+
+                  <span>- </span>
+                  <a
+                    className="underline"
+                    href="https://chrome.google.com/webstore/detail/clean-twitter/ibcjnfhpdjinbcmojnmpnokcgfljiebb?hl=en&authuser=0"
+                  >
+                    Clean Twitter
+                  </a>
+                  <br />
+
+                  <span style={{ borderRight: 'none' }}>- </span>
+                  <a
+                    className="underline"
+                    href="https://discord.com/oauth2/authorize?client_id=820275367083245578&permissions=0&scope=bot"
+                  >
+                    Zen Quotes
+                  </a>
+                </div>
+              );
             } else {
               return <span key={index}>{item}</span>;
             }
           })}
+        {completed && (
+          <div className="mt-4 gap-4 flex fadeIn">
+            <a className="underline" href="mailto:dhaiwatpandya@gmail.com">
+              Email
+            </a>
+            <a className="underline" href="https://github.com/Dhaiwat10">
+              GitHub
+            </a>
+            <a
+              className="underline"
+              href="https://linkedin.com/in/dhaiwat-pandya"
+            >
+              LinkedIn
+            </a>
+          </div>
+        )}
       </div>
     </main>
   );
