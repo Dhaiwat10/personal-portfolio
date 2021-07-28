8d74845a0951d88ae4909bb3a33661e11c954b94
Miral Suthar <57826091+miralsuthar@users.noreply.github.com>
2021-07-29 01:28:54 +0530

    fix the width issue in chrome

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,57 +1,57 @@
-import * as React from 'react';
-import cursor from '../images/cursor.gif';
-import avi from '../images/avi.jpeg';
-import { Counter, Meta } from '../components';
+import * as React from "react";
+import cursor from "../images/cursor.gif";
+import avi from "../images/avi.jpeg";
+import { Counter, Meta } from "../components";
 
 const Link = ({ label, href }) => (
-  <a className={`underline`} href={href} target='_blank' rel='noreferrer'>
+  <a className={`underline`} href={href} target="_blank" rel="noreferrer">
     {label}
   </a>
 );
 
 const Projects = () => (
-  <div style={{ marginBottom: '-18px' }} className='fadeIn'>
-    <span style={{ borderRight: 'none' }}>- </span>
+  <div style={{ marginBottom: "-18px" }} className="fadeIn">
+    <span style={{ borderRight: "none" }}>- </span>
     <Link
-      label='react-link-preview'
-      href='https://www.npmjs.com/package/@dhaiwat10/react-link-preview'
+      label="react-link-preview"
+      href="https://www.npmjs.com/package/@dhaiwat10/react-link-preview"
     />
     <br />
 
     <span>- </span>
-    <Link label='Supabase' href='https://github.com/supabase/supabase' />
+    <Link label="Supabase" href="https://github.com/supabase/supabase" />
     <br />
 
     <span>- </span>
-    <Link label='daily.dev' href='https://github.com/dailydotdev/apps' />
+    <Link label="daily.dev" href="https://github.com/dailydotdev/apps" />
     <br />
 
     <span>- </span>
     <Link
-      label='Project Lockdown'
-      href='https://github.com/TheIOFoundation/ProjectLockdown/'
+      label="Project Lockdown"
+      href="https://github.com/TheIOFoundation/ProjectLockdown/"
     />
     <br />
 
     <span>- </span>
     <Link
-      label='Ropsten Faucet'
-      href='https://github.com/dhaiwat10/ropsten-faucet'
+      label="Ropsten Faucet"
+      href="https://github.com/dhaiwat10/ropsten-faucet"
     />
     <br />
 
     <span>- </span>
-    <Link label='Zen' href='https://github.com/Dhaiwat10/zenquotes' />
+    <Link label="Zen" href="https://github.com/Dhaiwat10/zenquotes" />
     <br />
 
     <span>- </span>
-    <Link label='Barça Universal' href='https://barcauniversal.com' />
+    <Link label="Barça Universal" href="https://barcauniversal.com" />
     <br />
 
     <span>- </span>
     <Link
-      label='Clean Twitter'
-      href='https://github.com/dhaiwat10/clean-twitter'
+      label="Clean Twitter"
+      href="https://github.com/dhaiwat10/clean-twitter"
     />
   </div>
 );
@@ -63,23 +63,23 @@ const Blog = () => {
   return (
     <>
       <div>
-        <span className='fadeIn'>
-          {msg}{' '}
+        <span className="fadeIn">
+          {msg}{" "}
           <a
-            id='blog-link'
-            href='https://blog.dhaiwatpandya.com'
-            target='_blank'
-            rel='noreferrer'
+            id="blog-link"
+            href="https://blog.dhaiwatpandya.com"
+            target="_blank"
+            rel="noreferrer"
           >
             my blog
           </a>
           <br />
           {msg2}
           <a
-            id='blog-link'
-            href='https://www.youtube.com/watch?v=rZZItYsH8qg'
-            target='_blank'
-            rel='noreferrer'
+            id="blog-link"
+            href="https://www.youtube.com/watch?v=rZZItYsH8qg"
+            target="_blank"
+            rel="noreferrer"
           >
             speak at conferences
           </a>
@@ -87,14 +87,14 @@ const Blog = () => {
         </span>
       </div>
       <br />
-      <div className='flex gap-16 fadeIn'>
+      <div className="flex gap-16 fadeIn">
         <Counter
           number={200000}
-          text='users reached'
+          text="users reached"
           speed={5000}
-          color='#ff7092'
+          color="#ff7092"
         />
-        <Counter number={16000} text='readers reached' color='#ffde82' />
+        <Counter number={16000} text="readers reached" color="#ffde82" />
       </div>
     </>
   );
@@ -132,54 +132,54 @@ const IndexPage = () => {
   }, []);
 
   const scrollToBottom = () => {
-    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+    window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
   };
 
   const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   return (
     <main>
       <Meta />
       <div
-        id='container'
-        className='flex flex-col bg-accentedBlack font-mono text-white mx-auto p-4 sm:p-4 md:p-16 lg:px-32 lg:py-16 lg:flex-row xl:flex-row xl:px-64'
+        id="container"
+        className="flex flex-col bg-accentedBlack font-mono text-white mx-auto p-4 sm:p-4 md:p-16 lg:px-32 lg:py-16 lg:flex-row xl:flex-row xl:px-64"
         style={{
-          width: 'fit-content',
-          minHeight: '100vh',
-          fontSize: '18px',
-          maxHeight: 'fit-content',
+          width: "90%",
+          minHeight: "100vh",
+          fontSize: "18px",
+          maxHeight: "fit-content",
         }}
       >
         <img
-          className='fadeIn rounded-full h-32 mx-auto mb-8 lg:order-2 xl:order-2 lg:h-40 lg:ml-auto lg:mr-0 xl:h-64 xl:ml-auto xl:mr-0'
+          className="fadeIn rounded-full h-32 mx-auto mb-8 lg:order-2 xl:order-2 lg:h-40 lg:ml-auto lg:mr-0 xl:h-64 xl:ml-auto xl:mr-0"
           src={avi}
-          alt='Avatar'
+          alt="Avatar"
         />
-        <div className='content lg:order-1 xl:order-1'>
+        <div className="content lg:order-1 xl:order-1">
           {INPUT.slice(0, counter)
-            .split('')
+            .split("")
             .map((item, index) => {
-              if (item === '\n') {
+              if (item === "\n") {
                 return <br key={index} />;
-              } else if (item === '%') {
+              } else if (item === "%") {
                 return <Projects key={index} />;
-              } else if (item === '~') {
+              } else if (item === "~") {
                 return <Blog key={index} />;
               } else {
                 return item;
               }
             })}
-          <img alt='Cursor' src={cursor} id='cursor' />
+          <img alt="Cursor" src={cursor} id="cursor" />
           {completed && (
-            <div className='bottom-links mt-4 fadeIn'>
-              <Link label='Email' href='mailto:dhaiwatpandya@gmail.com' />
-              <Link label='GitHub' href='https://github.com/Dhaiwat10' />
-              <Link label='Blog' href='https://blog.dhaiwatpandya.com' />
+            <div className="bottom-links mt-4 fadeIn">
+              <Link label="Email" href="mailto:dhaiwatpandya@gmail.com" />
+              <Link label="GitHub" href="https://github.com/Dhaiwat10" />
+              <Link label="Blog" href="https://blog.dhaiwatpandya.com" />
               <Link
-                label='LinkedIn'
-                href='https://linkedin.com/in/dhaiwat-pandya'
+                label="LinkedIn"
+                href="https://linkedin.com/in/dhaiwat-pandya"
               />
             </div>
           )}
